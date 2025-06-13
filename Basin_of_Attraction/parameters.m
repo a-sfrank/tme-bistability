b6dd2505a48cc3ff30a7cd75592af8f246b98f23
Anna Frank <asfrank88@gmail.com>
2025-06-13 13:57:36 +0200

    parameter file was not updated

--- Basin_of_Attraction/parameters.m
+++ Basin_of_Attraction/parameters.m
@@ -3,12 +3,12 @@
 % Main contributors: Anna-Simone Frank, Kamila Larripa, Hwayeon Ruy and Susanna
 % Roeblitz
 
+
 function params=parameters(Case)
- 
-% Baseline parameters (together with Case =1):
+   
 
     params.pT=0.023;    %1/day; proliferation of tumor cells
-    params.K=0.5;	%max relative amount of macrophages compared to tumor cells
+    params.K=0.5;
     params.f=0.5;     %relative required tumor size to supports M2 polarization
     params.dT=0.01;       %1/day; macrophage independent tumor cell death rate
     params.r1=10;     %1/cells; contribution of M1 to the killing of tumor cells
@@ -35,57 +35,56 @@ function params=parameters(Case)
     params.alpha2m=0;
 
     if Case==1  %low
-        params.alpham2=0.01;
         params.alpha02=0.1;
-        params.alpha1m=0.001;
-        params.alpha01=1;
-    end
-
-    % Specific cases to distuinguish between monot and bi-stability:
-
-    if Case==6  %high
-        params.alpham2=0.01;
-        params.alpha02=1;
-        params.alpha1m=0.001;
-        params.alpha01=1;
-    end
-
-    if Case==3  %original bistable (medium/low)
-        params.alpham2=0.01;
-        params.alpha02=1;
-        params.alpha1m=0.1;
-        params.alpha01=10;
-        
+        params.alpha1m=0.001;       
     end
 
     if Case==2  %bistable
-        params.alpham2=0.01;
         params.alpha02=0.1;
-        params.alpha1m=0.1; %increased
-        params.alpha01=1;
+        params.alpha1m=0.1;	 %increased        
+        %params.g=0;
+    end
+
+    if Case==3  %high
+        params.alpha02=0.1;
+        params.alpha1m=0.5; 	%increased        
     end
 
     if Case==4  %bistable 
-        params.alpham2=0.01;
         params.alpha02=1;
         params.alpha1m=0.001;
-        params.alpha01=1;
-        params.alpha20=0.1; %changed
+        params.alpha20=0.1; 	%increased
     end
 
     if Case==5  %bistable
-        params.alpham2=0.01;
-        params.alpha02=0.5;
+        params.alpha02=0.5;	%increased
+        params.alpha1m=0.001;       
+    end
+
+    if Case==6  %high
+        params.alpha02=1;	%increased
         params.alpha1m=0.001;
-        params.alpha01=1;
     end
 
-    if Case==7  %high
-        params.alpham2=0.01;
-        params.alpha02=0.1;
-        params.alpha1m=0.5; %increased
-        params.alpha01=1;
+    if Case==7  %bistable 
+        params.alpha02=1;	%increased
+        params.alpha1m=0.001;
+        params.alpha01=2;	%increased
     end
 
+    if Case==8  %bistable 
+        params.alpha02=0.1;	
+        params.alpha1m=0.001;
+        params.alpha12=0.03;	%%increased
+    end
+
+    if Case==9  %high 
+        params.alpha02=0.1;	
+        params.alpha1m=0.001;
+	params.alpha12=0.1;	%%increased
+    end
+   
 
 end
+
+
